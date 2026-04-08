e72a5a65f42823028973ca7e793575cd1358318a
ThatN3rd <94089983+ThatN3rd@users.noreply.github.com>
2026-04-08 14:03:23 -0400

    Update cache URLs to use relative paths

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'program-track-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/app.js',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
